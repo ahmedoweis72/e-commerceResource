5d273c90792b2ac7c120de9ef9a14abcc0c36763
Ahmed <144798780+ahmedoweis72@users.noreply.github.com>
2024-10-15 18:12:51 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
+  },
+  plugins: [
+    flowbite.plugin(),
+  ],
+}
+
